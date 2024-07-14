8ec2abfdb7f5596c43d13dfc2af46e97ea577e43
Znackt <Znackt@proton.me>
2024-07-14 06:09:30 +0530

    added new question

--- conditional1/src/main.rs
+++ conditional1/src/main.rs
@@ -6,19 +6,14 @@ fn main() {
     let mut input1 = String::new();
     io::stdin().read_line(&mut input1);
     let mut input2 = String::new();
-    io::stdin().read_line(&mut input2)
+    io::stdin()
+        .read_line(&mut input2)
         .expect("Failed to read line");
-    /*Trim the input and parse it to an integer
+    //Trim the input and parse it to an integer
     let mut x: i32 = input1.trim().parse().expect("Please enter a valid integer");
-    let mut y: i32 = input2.trim().parse().expect("Please enter a valid integer"); */
-    /* let Some(ch) = input1.trim().chars().next() else {
-            println!("No valid character entered.");
-            return;
-    };
-    check_alphabet(ch); */
+    let mut y: i32 = input2.trim().parse().expect("Please enter a valid integer"); 
+    divisible(x);
 }
-
-//WAP TO TAKE POSITIVE INTEGER INPUT AND TELL IF IT'S EVEN OR NOT ?
 
 /* fn even_odd (x: i32) {
     if x % 2 == 0 {
@@ -27,8 +22,6 @@ fn main() {
         println!("{} is odd", x);
     }
 } */
-
-//GIVEN AN INT. PRINT THE ABSOLUTE VALUE OF THAT INTEGER?
 
 /* fn absolute_value(mut x: i32){
     if x < 0 {
@@ -39,9 +32,6 @@ fn main() {
         print!("{} is already positive",x);
     }
 } */
-
-/*IF CP AND SP OF AN ITEM IS INPUT THROUGH THE KEYBOARD. WRITE A PROGRAM IF SELLER HAS 
-MADE PROFIT FOR LOSS. ALSO DETERMINE HOW MUCH PROFIT OR LOSS INCURED.*/
 
 /*fn profit_loss(x: i32,y: i32){
 
@@ -57,8 +47,6 @@ MADE PROFIT FOR LOSS. ALSO DETERMINE HOW MUCH PROFIT OR LOSS INCURED.*/
     }
 }*/
 
-//Take positive no. Input and tell if it is 3-digit no. or not!
-
 /*fn check_three_digit (x: i32) {
 
     if (x > 100) && (x < 999) {
@@ -68,8 +56,6 @@ MADE PROFIT FOR LOSS. ALSO DETERMINE HOW MUCH PROFIT OR LOSS INCURED.*/
         println!("{} is not a 3-Digit number",x);
     }
 }*/
-
-//Take positive integer input and tell if it's divisible by 5 and 3
 
 /*fn divisible (x: i32) {
     if (x % 3 == 0) && (x % 5 == 0) {
@@ -83,14 +69,19 @@ MADE PROFIT FOR LOSS. ALSO DETERMINE HOW MUCH PROFIT OR LOSS INCURED.*/
     }
 }*/
 
-//To check whether character is an alphabet or not 
-
-
 /*fn check_alphabet(ascii: char) {
-    match ascii {
-          'A'..='Z' => println!("The character you entered is uppercase alphabet"),
-          'a'..='z' => println!("The character you entered is lowercase alphabet"),
-          _         => println!("The character you entered is not an alphabet"),
-        }
-    }*/
-    
+match ascii {
+      'A'..='Z' => println!("The character you entered is uppercase alphabet"),
+      'a'..='z' => println!("The character you entered is lowercase alphabet"),
+      _         => println!("The character you entered is not an alphabet"),
+    }
+}*/
+
+fn divisible (x:i32) {
+    if (x % 3 == 0) || (x % 5 == 0) {
+        println!("The number you entered is either divisible by 5 or 3");
+    }
+    else {
+        println!("The number you entered is neither divisible by 5 or 3");
+    }
+}
