f7fd71e6b81d78eaca83a78db8a43c8c0d7e8344
Znackt <Znackt@proton.me>
2024-07-24 19:30:04 +0530

    fixed err

--- loops1/src/main.rs
+++ loops1/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut input= String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     let x: i32 = input.trim().parse().expect("Please enter a valid integer");
-    even_one_to_hundred(x);
+    
 }
 
 //Print hello world 'n' times. Take 'n' user input!
